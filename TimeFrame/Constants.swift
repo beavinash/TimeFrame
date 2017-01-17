c1f155bc2fc5afc702e22c5e637e976c37c9e35a
Avinash Reddy <beavinash15@gmail.com>
2017-01-17 11:31:57 -0500

    Basic setup

---
+++ TimeFrame/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TimeFrame
+//
+//  Created by Avinash Reddy on 1/17/17.
+//  Copyright © 2017 theEine. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ TimeFrame/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  TimeFrame
+//
+//  Created by Avinash Reddy on 1/17/17.
+//  Copyright © 2017 theEine. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
