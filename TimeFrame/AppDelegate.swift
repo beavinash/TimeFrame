9b98b425090cae594e6f5c22b0810e27a2b632bb
Avinash Reddy <beavinash15@gmail.com>
2017-01-17 16:42:24 -0500

    Customization of Nav bar and few other changes in AppDelegate

--- TimeFrame/AppDelegate.swift
+++ TimeFrame/AppDelegate.swift
@@ -17,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 5.0/255.0, green: 154.0/255.0, blue: 185.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.white
+        
+        if let barFont = UIFont(name: "AvenirNextCondensed-DemiBold", size: 24.0) {
+            UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white, NSFontAttributeName:barFont]
+        }
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+        
         return true
     }
 
